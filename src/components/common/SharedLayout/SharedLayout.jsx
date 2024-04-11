be46eaafe8703ee7803a986cc82ac4c73a469506
Artem Tokarev <tokarev.artem.v@gmail.com>
2024-04-12 01:51:37 +0300

    added Container SharedLayout global styles

--- src/components/common/SharedLayout/SharedLayout.jsx
+++ src/components/common/SharedLayout/SharedLayout.jsx
@@ -1,5 +1,24 @@
+import { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
+import Container from '../Container/Container';
+import Header from '../Header/Header';
+import Spinner from '../Spinner/Spinner';
+
+import s from './SharedLayout.module.css';
+
 const SharedLayout = () => {
-  return <div>SharedLayout</div>;
+  return (
+    <>
+      <Header />
+      <Suspense fallback={<Spinner />}>
+        <main style={{ height: '100%' }}>
+          <Container>
+            <Outlet />
+          </Container>
+        </main>
+      </Suspense>
+    </>
+  );
 };
 
 export default SharedLayout;
