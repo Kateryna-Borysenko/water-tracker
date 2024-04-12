0ea0e7b75af5194abe3fe99b3fdc555bb5f02c13
KaterynaPryimakova <134732205+KaterynaPryimakova@users.noreply.github.com>
2024-04-12 17:50:17 +0300

    added Header component with Logo, UserAuth, UserLogo and UserLogoModal components

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -1,11 +1,20 @@
+import Container from '../Container/Container';
 import Logo from './Logo/Logo';
+import UserAuth from './UserAuth/UserAuth';
+import UserLogo from './UserLogo/UserLogo';
 import s from './Header.module.css';
 
 const Header = () => {
   return (
-    <header className={s.header}>
-      <Logo />
-    </header>
+    <Container>
+      <header className={s.header}>
+        <Logo />
+
+        <UserAuth />
+
+        {/* <UserLogo /> */}
+      </header>
+    </Container>
   );
 };
 
---
+++ src/components/common/Header/UserLogo/UserLogoModal/UserLogoModal.jsx
@@ -0,0 +1,21 @@
+import settingIcon from '../../../../../assets/static/settings.svg';
+import exitIcon from '../../../../../assets/static/logout.svg';
+import s from './UserLogoModal.module.css';
+
+const UserLogoModal = () => {
+  return (
+    <ul className={s.headerModal}>
+      <li className={s.item}>
+        <img width="16" height="16" src={settingIcon} alt="Icon setting" />
+        <p>Setting</p>
+      </li>
+
+      <li className={s.item}>
+        <img width="16" height="16" src={exitIcon} alt="Icon log out" />
+        <p>Log out</p>
+      </li>
+    </ul>
+  );
+};
+
+export default UserLogoModal;
