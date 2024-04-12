db3925a3a884b082e2bfa6eb84f999be092b9177
GermanSTr <gershev1994@gmail.com>
2024-04-12 23:19:27 +0300

    add icon to button

--- src/components/common/Button/Button.jsx
+++ src/components/common/Button/Button.jsx
@@ -1,10 +1,22 @@
 import s from './Button.module.css';
 
-const Button = ({ type = 'button', title, className, onClick, ...rest }) => {
+const Button = ({
+  type = 'button',
+  title,
+  icon,
+  className,
+  onClick,
+  ...rest
+}) => {
   const buttonClasses = `${s.button} ${s[className]}`;
 
   return (
     <button type={type} onClick={onClick} className={buttonClasses} {...rest}>
+      {icon && (
+        <svg>
+          <use href={icon}></use>
+        </svg>
+      )}
       {title}
     </button>
   );
