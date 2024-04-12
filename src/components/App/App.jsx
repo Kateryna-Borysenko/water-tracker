9670751e3e3a32609103c3ca756dc7892b22ab69
KaterynaPryimakova <134732205+KaterynaPryimakova@users.noreply.github.com>
2024-04-12 23:23:05 +0300

    added SharedLayout route

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,20 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import WelcomePage from '../../pages/WelcomePage/WelcomePage';
 import 'react-toastify/dist/ReactToastify.css';
 import '../../assets/styles/global.module.css';
 import SignupPage from '../../pages/SignupPage/SignupPage';
+import SharedLayout from '../common/SharedLayout/SharedLayout';
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<WelcomePage />}></Route>
-        <Route path="/signup" element={<SignupPage />}></Route>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<WelcomePage />}></Route>
+          <Route path="/signup" element={<SignupPage />}></Route>
+          <Route path="*" element={<Navigate to="/" />} />
+        </Route>
       </Routes>
 
       <ToastContainer position="top-right" autoClose={5000} theme="light" />
