79221ee13b92e0bc720e79e1b3a2c18f7d12159f
Lolita Yenik <lolitayenik@ukr.net>
2024-04-14 15:09:11 +0300

    finish setting-card css add eye btn

---
+++ src/components/SettingCard/EyeBtn/EyeBtn.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import OpenedEyeIcon from '../../../assets/static/icons/eye.svg?react';
+import ClosedEyeIcon from '../../../assets/static/icons/eye-slash.svg?react';
+import s from './EyeBtn.module.css';
+
+const EyeBtn = () => {
+  const [isOpenEye, setIsOpenEye] = useState(false);
+
+  const handleToggleEye = e => {
+    e.preventDefault();
+    if (isOpenEye) {
+      setIsOpenEye(false);
+      e.currentTarget.previousElementSibling.type = 'password';
+    }
+    if (!isOpenEye) {
+      setIsOpenEye(true);
+      e.currentTarget.previousElementSibling.type = 'text';
+    }
+  };
+  return (
+    <>
+      <button className={s.eyeBtn} onClick={handleToggleEye}>
+        {!isOpenEye ? (
+          <ClosedEyeIcon width="16" height="16" />
+        ) : (
+          <OpenedEyeIcon width="16" height="16" />
+        )}
+      </button>
+    </>
+  );
+};
+export default EyeBtn;
--- src/components/SettingCard/SettingCard.jsx
+++ src/components/SettingCard/SettingCard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { updateAvatar, updateUserData } from '../../redux/user/userOperations';
 import {
@@ -19,6 +19,7 @@ import {
   EMAIL_REGEX,
   PASSWORD_REGEX,
 } from '../../helpers/regexPatterns';
+import EyeBtn from './EyeBtn/EyeBtn';
 
 //formik
 
@@ -41,137 +42,168 @@ const SettingCard = () => {
     <div className={s.container}>
       <h2 className={s.title}>Setting</h2>
 
-      {/* <form className={s.form}> */}
-      <div className={s.wrapper}>
-        {/* <div className={s.leftBox}> */}
+      <form className={s.form}>
+        <div className={s.wrapper}>
+          <div className={s.leftBox}>
+            <h3 className={` ${s.item} ${s.mediumText} ${s.smallMb}`}>
+              Your photo
+            </h3>
+            {/* -----------аватар----- */}
 
-        <h3 className={` ${s.item} ${s.mediumText} ${s.smallMb}`}>
-          Your photo
-        </h3>
-        {/* -----------аватар----- */}
+            <div className={s.avatarWrapper}>
+              <span className={s.avatar}>
+                {avatarURL || avatar ? (
+                  <img src={avatarURL || avatar} alt="avatar" />
+                ) : (
+                  <p>{defaultAvatarFirstLetter}</p>
+                )}
+              </span>
+              {/* -----------upload a photo----- */}
 
-        <div className={s.avatarWrapper}>
-          <span className={s.avatar}>
-            {avatarURL || avatar ? (
-              <img src={avatarURL || avatar} alt="avatar" />
-            ) : (
-              <p>{defaultAvatarFirstLetter}</p>
-            )}
-          </span>
-          {/* -----------upload a photo----- */}
-          {/* <form ></form> */}
-          <div className={s.fileWrapper}>
-            <input
-              type="file"
-              id="upload"
-              name="file"
-              className={s.visuallyHidden}
-            />
-            <label htmlFor="upload" className={s.uploadingWrapper}>
-              {/* <img src={uploadingSVG} alt="uploading-icon" /> */}
-              <UploadingIcon className={s.uploadingIcon} />
+              <div className={s.fileWrapper}>
+                <input
+                  type="file"
+                  id="upload"
+                  name="file"
+                  className={s.visuallyHidden}
+                />
+                <label htmlFor="upload" className={s.uploadingWrapper}>
+                  {/* <img src={uploadingSVG} alt="uploading-icon" /> */}
+                  <UploadingIcon className={s.uploadingIcon} />
 
-              <span className={s.uploadingText}>Upload a photo</span>
+                  <span className={s.uploadingText}>Upload a photo</span>
+                </label>
+              </div>
+            </div>
+
+            {/* </div> */}
+
+            {/* -----------genderBox */}
+            <h3 className={`${s.item} ${s.mediumText} ${s.mediumMb}`}>
+              Your gender identity
+            </h3>
+
+            <div className={s.genderBox}>
+              <label className={` ${s.labelContainer} ${s.smallText} `}>
+                <input
+                  type="radio"
+                  value="female"
+                  name="gender"
+                  className={s.checkbox}
+                />
+                Woman
+                <span className={s.checkmark}></span>
+              </label>
+
+              <label className={` ${s.labelContainer} ${s.smallText} `}>
+                <input
+                  type="radio"
+                  value="male"
+                  name="gender"
+                  className={s.checkbox}
+                />
+                Man
+                <span className={s.checkmark}></span>
+              </label>
+            </div>
+
+            <label
+              htmlFor="yourName"
+              className={` ${s.item} ${s.mediumText} ${s.smallMb}`}
+            >
+              Your name
             </label>
+            <input
+              id="yourName"
+              type="text"
+              name="name"
+              placeholder="Name"
+              pattern={USER_NAME_REGEX}
+              className={`${s.input} ${s.largeMb} `}
+            />
+
+            <label
+              htmlFor="email"
+              className={`${s.item} ${s.mediumText} ${s.smallMb}`}
+            >
+              E-mail
+            </label>
+            <input
+              id="email"
+              type="email"
+              name="email"
+              placeholder="E-mail"
+              pattern={EMAIL_REGEX}
+              className={`${s.input} ${s.largeMb} ${s.smallText}`}
+            />
           </div>
-        </div>
 
-        {/* </div> */}
+          {/* -----right box desk ------ */}
+          <div className={s.rightBox}>
+            <h3 className={`${s.item} ${s.mediumText} ${s.smallMb}`}>
+              Password
+            </h3>
 
-        {/* -----------genderBox */}
+            <label
+              htmlFor="outdatedPassword"
+              className={`${s.itemText} ${s.smallText}`}
+            >
+              Outdated password:
+            </label>
+            <div className={`${s.inputWrapper} ${s.passwordInput}`}>
+              <input
+                id="outdatedPassword"
+                type="password"
+                name="password"
+                placeholder="Password"
+                pattern={PASSWORD_REGEX}
+                className={`${s.input}  `}
+              />
+              <EyeBtn />
+            </div>
 
-        <h3 className={`${s.item} ${s.mediumText} ${s.mediumMb}`}>
-          Your gender identity
-        </h3>
-        {/* <form action=""></form> */}
-        <div className={s.genderBox}>
-          <label className={` ${s.labelContainer} ${s.smallText} `}>
-            <input
-              type="radio"
-              value="female"
-              name="gender"
-              className={s.checkbox}
-            />
-            Woman
-            <span className={s.checkmark}></span>
-          </label>
+            <label
+              htmlFor="newPassword"
+              className={` ${s.itemText} ${s.smallText}`}
+            >
+              New password:
+            </label>
+            <div className={`${s.inputWrapper} ${s.passwordInput}`}>
+              <input
+                id="newPassword"
+                type="password"
+                name="newPassword"
+                placeholder="Password"
+                pattern={PASSWORD_REGEX}
+                className={`${s.input} `}
+              />
+              <EyeBtn />
+            </div>
 
-          <label className={` ${s.labelContainer} ${s.smallText} `}>
-            <input
-              type="radio"
-              value="male"
-              name="gender"
-              className={s.checkbox}
-            />
-            Man
-            <span className={s.checkmark}></span>
-          </label>
-        </div>
-
-        <label className={` ${s.item} ${s.mediumText} ${s.smallMb}`}>
-          Your name
-        </label>
-        <input
-          type="text"
-          name="name"
-          placeholder="Name"
-          pattern={USER_NAME_REGEX}
-          className={`${s.input} ${s.largeMb} `}
-        />
-
-        <label className={`${s.item} ${s.mediumText} ${s.smallMb}`}>
-          E-mail
-        </label>
-        <input
-          type="email"
-          name="email"
-          placeholder="E-mail"
-          pattern={EMAIL_REGEX}
-          className={`${s.input} ${s.largeMb} ${s.smallText}`}
-        />
-
-        {/* -----right box desk ------ */}
-        <div className={s.rightBox}>
-          <h3 className={`${s.item} ${s.mediumText} ${s.smallMb}`}>Password</h3>
-          <label className={`${s.itemText} ${s.smallText}`}>
-            Outdated password:
-          </label>
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            pattern={PASSWORD_REGEX}
-            className={`${s.input} ${s.passwordInput} `}
-          />
-
-          <label className={` ${s.itemText} ${s.smallText}`}>
-            New password:
-          </label>
-          <input
-            type="password"
-            name="newPassword"
-            placeholder="Password"
-            pattern={PASSWORD_REGEX}
-            className={`${s.input} ${s.passwordInput}`}
-          />
-
-          <label className={` ${s.itemText} ${s.smallText}`}>
-            Repeat new password:
-          </label>
-          <input
-            type="password"
-            name="repeatedPassword"
-            placeholder="Password"
-            pattern={PASSWORD_REGEX}
-            className={`${s.input} ${s.passwordInput}`}
-          />
+            <label
+              htmlFor="repeatPassword"
+              className={` ${s.itemText} ${s.smallText}`}
+            >
+              Repeat new password:
+            </label>
+            <div className={`${s.inputWrapper} ${s.largeMb}`}>
+              <input
+                id="repeatPassword"
+                type="password"
+                name="repeatedPassword"
+                placeholder="Password"
+                pattern={PASSWORD_REGEX}
+                className={`${s.input}  `}
+              />
+              <EyeBtn />
+            </div>
+          </div>
         </div>
 
         <button type="submit" aria-label="Save changes" className={s.saveBtn}>
           Save
         </button>
-      </div>
-      {/* </form> */}
+      </form>
     </div>
   );
 };
