da29bd54bc73c0f8425b12dfac362bcd8f288c14
BoikoAlona <a.semikhova@gmail.com>
2024-04-15 20:18:56 +0300

    revision after review

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,60 +1,26 @@
-import iconCalendar from '../../assets/static/calendar.svg';
-import iconChart from '../../assets/static/chart.svg';
-import iconTools from '../../assets/static/tools.svg';
-
 import s from './Item.module.css';
 
-const itemArrOptions = [
-  {
-    svg: iconCalendar,
-    text: 'Habit drive',
-  },
-  {
-    svg: iconChart,
-    text: 'View statistics',
-  },
-  {
-    svg: iconTools,
-    text: 'Personal rate setting',
-  },
-];
-
-const itemArrayBenefits = [
-  'Supply of nutrients to all organs',
-  'Providing oxygen to the lungs',
-  'Maintaining the work of the heart',
-  'Release of processed substances',
-  'Ensuring the stability of the internal environment',
-  'Maintaining within the normal temperature',
-  'Maintaining an immune system capable of resisting disease',
-];
-
-const Item = () => {
+const Item = ({ array, className }) => {
   return (
-    <div className={s.itemContainer}>
-      <div className={s.itemContainerOptions}>
-        <h4 className={s.description}>Tracker Benefits</h4>
-        <ul className={s.list}>
-          {itemArrOptions.map((item, index) => (
-            <li className={s.listItemOptions} key={index}>
+    <ul className={`${s.list} ${s[className]}`}>
+      {array.map((item, index) => (
+        <li className={`${s.listItemBenefits} ${s.listItemOptions} ${s[className]}`} key={index}>
+          {item.svg ? (
+            <>
               <img src={item.svg} alt="Icon" />
               {item.text}
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className={s.itemContainerBenefits}>
-        <h4 className={s.description}>Why drink water</h4>
-        <ul className={`${s.list} ${s.listBenefits}`}>
-          {itemArrayBenefits.map((item, index) => (
-            <li className={s.listItemBenefits} key={index}>
+            </>
+          ) : (
+            <span className={s.listItemOptions}>
+              <span className={s.point}></span>
               {item}
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
+            </span>
+          )}
+        </li>
+      ))}
+    </ul>
   );
 };
 
 export default Item;
+
