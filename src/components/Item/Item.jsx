e3a49db01c85e0806aa7ef762f9195a4d83e68b3
BoikoAlona <a.semikhova@gmail.com>
2024-04-16 02:44:46 +0300

    corrected general and specific styles, deleted span tag

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -2,19 +2,16 @@ import s from './Item.module.css';
 
 const Item = ({ array, className }) => {
   return (
-    <ul className={`${s.list} ${s[className]}`}>
+    <ul className={s.listItems}>
       {array.map((item, index) => (
-        <li className={`${s.listItemBenefits} ${s.listItemOptions} ${s[className]}`} key={index}>
+        <li className={`${s.list} ${s[className]}`} key={index}>
           {item.svg ? (
             <>
               <img src={item.svg} alt="Icon" />
               {item.text}
             </>
           ) : (
-            <span className={s.listItemOptions}>
-              <span className={s.point}></span>
-              {item}
-            </span>
+            <>{item}</>
           )}
         </li>
       ))}
@@ -23,4 +20,3 @@ const Item = ({ array, className }) => {
 };
 
 export default Item;
-
