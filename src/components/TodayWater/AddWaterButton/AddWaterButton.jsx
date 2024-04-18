8194608072a070d33c2abc3a458cd4fdc6b9badd
Maks <maks135254@gmail.com>
2024-04-19 00:06:17 +0300

    added structurs, style, some logic for btns

---
+++ src/components/TodayWater/AddWaterButton/AddWaterButton.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import Modal from '../../common/Modal/Modal';
+import AddWaterModal from '../../AddWaterModal/AddAndEditWaterCard';
+import Icons from '../../Icons/Icons';
+import s from './AddWaterButton.module.css';
+
+export const AddWaterButton = () => {
+    const [isOpenModal, setIsOpenModal] = useState(false);
+
+    const handleCloseModal = () => {
+        setIsOpenModal(false)
+    }
+
+    return (
+        <>
+        <button  
+            className={s.addWaterBtn} 
+            onClick={() => setIsOpenModal(true)}
+        >
+            <Icons id={'plus'} size={24} className={'iconPlus'} /> 
+            Add water
+        </button>
+
+        {isOpenModal && (
+        <Modal onClose={handleCloseModal} >
+            <AddWaterModal isEditable={false}></AddWaterModal>
+        </Modal> 
+        )}
+        </>
+        
+    );
+};
+
+export default AddWaterButton;
---
+++ src/components/TodayWater/TodayWaterItem/TodayWaterItem.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+import { format } from 'date-fns';
+import Modal from '../../common/Modal/Modal';
+import AddWaterModal from '../../AddWaterModal/AddAndEditWaterCard';
+import Icons from '../../Icons/Icons';
+import s from './TodayWaterItem.module.css'
+
+
+export const TodayWaterItem = ({ waterVolume, time }) => {
+    const [isOpenModalEdit, setIsOpenModalEdit] = useState(false);
+    const [isOpenModalDelete, setIsOpenModalDelete] = useState(false);
+
+    const handleCloseModal = () => {
+        setIsOpenModalEdit(false);
+        setIsOpenModalDelete(false);
+    };
+
+    const formatTime = (dateTimeStr) => {
+        const date = new Date(dateTimeStr);
+        return format(date, 'h:mm a');
+    };
+
+    return (
+        <>
+            <li className={s.todayWaterItem}>
+                <div className={s.wrapItemLeft}>
+                    <Icons id={'glass'} className={s.glassIcon} />
+                    <p className={s.textWaterAmount}>{waterVolume} ml</p>
+                    <p className={s.textTime}>{formatTime(time)}</p>
+                </div>
+
+                <div className={s.wrapItemRight}>
+                    <button className={s.pencilBtn} onClick={() => setIsOpenModalEdit(true)}>
+                        <Icons id={'edit-pencil'} className={s.iconPencil} />
+                    </button>
+
+                    <button className={s.trashBtn} onClick={() => setIsOpenModalDelete(true)}>
+                        <Icons id={'trash'} stroke={'#EF5050'} />
+                    </button>
+                </div>
+            </li>
+
+            {isOpenModalDelete && (
+                <Modal onClose={handleCloseModal}>
+                    <ManagementCard
+                        title="Delete entry"
+                        description="Are you sure you want to delete the entry?"
+                        secondButton="Delete"
+                        className="alignRight"
+                    />
+                </Modal>
+            )}
+
+            {isOpenModalEdit && (
+            <Modal onClose={handleCloseModal} >
+                <AddWaterModal isEditable={true}></AddWaterModal>
+            </Modal> 
+            )}
+        </>
+    );
+};
+
+export default TodayWaterItem;
