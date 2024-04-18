0c20b8e74648f9e3279a8a274113a2d7f0a05847
Artem Tokarev <tokarev.artem.v@gmail.com>
2024-04-18 15:04:13 +0300

    after review

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,9 @@
 import Container from '../../components/common/Container/Container';
 import Calendar from '../../components/Calendar/Calendar';
 import Meta from '../../components/common/Meta/Meta';
-import Modal from '../../components/common/Modal/Modal';
-import AddAndEditWaterCard from '../../components/AddWaterModal/AddAndEditWaterCard';
 import bottle from '../../assets/static/image/bottle-home-screen-M-2x.png';
 import s from './HomePage.module.css';
+import ProgressBar from '../../components/ProgressBarHomePage/ProgressBar';
 
 const HomePage = () => {
   return (
@@ -16,14 +15,12 @@ const HomePage = () => {
             <div className={s.contentContainer}>
               <img src={bottle} className={s.bottle} alt="Bottle of Water" />
             </div>
+            <ProgressBar />
             <div className={s.homePageWidgetWrapper}>
               <Calendar />
             </div>
           </div>
         </Container>
-        <Modal>
-          <AddAndEditWaterCard />
-        </Modal>
       </div>
     </>
   );
