d42936a360d52e587056f09216f83cd65a19e48d
BoikoAlona <a.semikhova@gmail.com>
2024-04-19 19:32:36 +0300

    added traslation and fixed background

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -3,12 +3,15 @@ import MainTitle from '../../components/common/MainTitle/MainTitle';
 import Subtitle from '../../components/common/Subtitle/Subtitle';
 import Item from '../../components/Item/Item';
 import Button from '../../uikit/Button/Button';
+import LangsSwitcher from '../../components/LangsSwitcher/LangsSwitcher';
 import Meta from '../../components/common/Meta/Meta';
 
 import {
   itemArrayBenefits,
-  itemArrOptions,
+  itemArrayOptions,
 } from '../../components/Item/itemArrays';
+
+import { useTranslation } from 'react-i18next';
 
 import s from '../WelcomePage/WelcomePage.module.css';
 import { useNavigate } from 'react-router-dom';
@@ -19,35 +22,48 @@ const WelcomePage = () => {
   const handleClick = () => {
     navigate('/signup');
   };
+  const { t } = useTranslation();
+
+  const translatedBenefits = itemArrayBenefits.map((item, index) => ({
+    svg: item.svg,
+    text: t(`WelcomePage.itemArrayBenefits.benefit${index + 1}`),
+  }));
+
+  const translatedOptions = itemArrayOptions.map((item, index) =>
+    t(`WelcomePage.itemArrayOptions.option${index + 1}`),
+  );
 
   return (
     <div className={s.container}>
       <Meta />
-
-      <Container className={s.containerWelcomePage}>
+      <Container className="containerWelcomePage">
+        <LangsSwitcher />
         <div className={s.contentContainer}>
           <div className={s.itemContainerBenefits}>
             <MainTitle
-              title="Water consumption tracker"
+              title={t('WelcomePage.mainTitle')}
               className="mainTitleWelcomePage"
             />
-            <p className={s.slogan}>Record daily water intake and track</p>
+            <p className={s.slogan}>{t('WelcomePage.slogan')}</p>
             <Subtitle
-              title="Tracker Benefits"
+              title={t('WelcomePage.subtitleBenefits')}
               className="subtitleWelcomePage"
             />
-            <Item array={itemArrayBenefits} className="listItemBenefits" />
+            <Item array={translatedBenefits} className="listItemBenefits" />
             <div className={s.tryTrackerButton}>
               <Button
                 onClick={handleClick}
                 className="tryTrackerButton"
-                title="Try tracker"
+                title={t('WelcomePage.tryTreckerButton')}
               />
             </div>
           </div>
           <div className={s.itemContainerOptions}>
-            <Subtitle title="Why drink water" className="subtitleWelcomePage" />
-            <Item array={itemArrOptions} className="listItemOptions" />
+            <Subtitle
+              title={t('WelcomePage.subtitleOptions')}
+              className="subtitleWelcomePage"
+            />
+            <Item array={translatedOptions} className="listItemOptions" />
           </div>
         </div>
       </Container>
