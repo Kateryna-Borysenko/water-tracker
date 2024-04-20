df4963a08f22c53b9dc95d9c5f066ef18db46b1c
Kateryna <k.borysenko.kyiv@gmail.com>
2024-04-20 14:17:27 +0300

    fix: aligns content vertically centered

---
+++ src/pages/ForgotPasswordPage/ForgotPasswordPage.jsx
@@ -0,0 +1,7 @@
+import s from './ForgotPasswordPage.module.css';
+
+const ForgotPasswordPage = () => {
+  return <div className={s.container}>ForgotPasswordPage</div>;
+};
+
+export default ForgotPasswordPage;
