35589156e3d238c86ea3f5f084863441cb3e9847
Artem <135842119+TokarevArtemV@users.noreply.github.com>
2024-04-22 19:14:45 +0300

    Update MyDailyNormaButton.jsx

--- src/components/MyDailyNorma/MyDailyNormaButton/MyDailyNormaButton.jsx
+++ src/components/MyDailyNorma/MyDailyNormaButton/MyDailyNormaButton.jsx
@@ -1,38 +1,38 @@
-// import { useState } from 'react';
-// import Modal from '../../common/Modal/Modal';
-// import MyDailyNormaModal from '../MyDailyNormaModal/MyDailyNormaModal';
-// import s from './MyDailyNormaButton.module.css';
-// import { useDispatch } from 'react-redux';
-// import { sentWaterRate } from '../../../redux/auth/authOperations';
+import { useState } from 'react';
+import Modal from '../../common/Modal/Modal';
+import MyDailyNormaModal from '../MyDailyNormaModal/MyDailyNormaModal';
+import s from './MyDailyNormaButton.module.css';
+import { useDispatch } from 'react-redux';
+import { sentWaterRate } from '../../../redux/auth/authOperations';
 
-// export const MyDailyNormaButton = () => {
-//   const dispatch = useDispatch();
-//   const [isOpenModal, setIsOpenModal] = useState(false);
+export const MyDailyNormaButton = () => {
+  const dispatch = useDispatch();
+  const [isOpenModal, setIsOpenModal] = useState(false);
 
-//   const handleCloseModal = () => {
-//     setIsOpenModal(prevState => !prevState);
-//   };
+  const handleCloseModal = () => {
+    setIsOpenModal(prevState => !prevState);
+  };
 
-//   return (
-//     <>
-//       <button
-//         type="button"
-//         className={s.editButton}
-//         onClick={() => {
-//           setIsOpenModal(prevState => !prevState);
-//           dispatch(sentWaterRate({ waterRate: 1900 }));
-//         }}
-//       >
-//         Edit
-//       </button>
+  return (
+    <>
+      <button
+        type="button"
+        className={s.editButton}
+        onClick={() => {
+          setIsOpenModal(prevState => !prevState);
+          dispatch(sentWaterRate({ waterRate: 1900 }));
+        }}
+      >
+        Edit
+      </button>
 
-//       {isOpenModal && (
-//         <Modal onClose={handleCloseModal}>
-//           <MyDailyNormaModal onClose={handleCloseModal} />
-//         </Modal>
-//       )}
-//     </>
-//   );
-// };
+      {isOpenModal && (
+        <Modal onClose={handleCloseModal}>
+          <MyDailyNormaModal onClose={handleCloseModal} />
+        </Modal>
+      )}
+    </>
+  );
+};
 
-// export default MyDailyNormaButton;
+export default MyDailyNormaButton;
