2192f3db019e13bf59f87884e710f755325df85c
Artem Tokarev <tokarev.artem.v@gmail.com>
2024-04-22 18:24:35 +0300

    fix myDailyNorma

--- src/components/MyDailyNorma/MyDailyNorma.jsx
+++ src/components/MyDailyNorma/MyDailyNorma.jsx
@@ -1,14 +1,19 @@
+import { useSelector } from 'react-redux';
 import Subtitle from '../common/Subtitle/Subtitle';
 import MyDailyNormaButton from './MyDailyNormaButton/MyDailyNormaButton';
+import { selectWaterRate } from '../../redux/auth/authSelectors';
 import s from './MyDailyNorma.module.css';
 
 const MyDailyNorma = () => {
+  const waterRate = useSelector(selectWaterRate);
+  const DailyNormaL = waterRate / 1000;
+
   return (
     <>
       <div className={s.dailyNormaContainer}>
-        <Subtitle title={'My daily norma'}></Subtitle>
+        <Subtitle title="My daily norma" />
         <div className={s.normaValue}>
-          <span>2 L</span>
+          <span>{DailyNormaL} L</span>
           <MyDailyNormaButton />
         </div>
       </div>
